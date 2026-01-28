262f962e7563ad93c132c4010d4e9244bf4f2ffe
Ayushka <27ayushkat@gmail.com>
2026-01-28 14:47:04 -0500

    hour 2 push

---
+++ main.js
@@ -0,0 +1,12 @@
+
+document.getElementById("equipment").addEventListener("click", () => 
+{ windows.location.href=portfolio.html});
+
+document.getElementById("skill-tree").addEventListener("click", () => 
+{ windows.location.href=portfolio.html});
+
+document.getElementById("save-slot").addEventListener("click", () => 
+{ windows.location.href=portfolio.html});
+
+document.getElementById("achievements").addEventListener("click", () => 
+{ windows.location.href=portfolio.html});
